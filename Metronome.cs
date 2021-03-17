0a802690a6c5dc1546e4982d53d7d1cc7a4ef66d
qtran9775 <77847133+qtran9775@users.noreply.github.com>
2021-03-17 09:17:23 -0500

    Add files via upload

---
+++ Metronome.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class Metronome
+{
+    float tempo;
+    int beat;
+    Boolean isPlaying;
+}
+
---
+++ Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class Note
+{
+    String ID;
+    double frequency;
+    int octave;
+
+    public Note(String id, double f, int o)
+    {
+        ID = id;
+        frequency = f;
+        octave = o;
+    }
+}
+
