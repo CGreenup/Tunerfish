1d0633e5a4e89de1a5df14b3644d71d24956e953
qtran9775 <77847133+qtran9775@users.noreply.github.com>
2021-04-07 09:53:41 -0500

    Add files via upload
    Just a temp file, so we know what serializing in our case supposed to look like. These codes should be in the Tuning Feature file

---
+++ TestForSel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+
+namespace TunerFishTest
+{
+    class TestForSel
+    {
+        public void TestForSelMain()
+        {
+            String fileAddress = @"C:\Users\qtran\source\repos\TunerFishTest\TunerFishTest\Test1.json";
+            List<Event> Events = new List<Event>();
+            for (int i = 0; i < 4; i++)
+            {
+                Event newEvent = new Event();
+                newEvent.date = DateTime.Now;
+                newEvent.centOff = i;
+                newEvent.note = "A";
+
+                Events.Add(newEvent);
+            }
+
+            string json = JsonConvert.SerializeObject(Events, Formatting.Indented);
+            File.WriteAllText(fileAddress, json);
+        }
+    }
+}
