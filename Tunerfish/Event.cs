d9f867ff64d6edd06766e9109dadccc5f193d30b
qtran9775 <77847133+qtran9775@users.noreply.github.com>
2021-04-22 11:11:42 -0500

    Adding HistoryForm and Event to Github

---
+++ Tunerfish/Event.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TunerFish
+{
+    class Event
+    {
+        public DateTime date { get; set; }
+        public String note { get; set; }
+        public int centOff { get; set; }
+    }
+}
---
+++ Tunerfish/HistoryForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Tunerfish
+{
+    public partial class HistoryForm : Form
+    {
+        String fileAddress = Path.Combine(Directory.GetCurrentDirectory(), "Test1.json");
+        public HistoryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void HistoryForm_Load(object sender, EventArgs e)
+        {
+            //Create Data Table
+            DataTable HistoryTable = new DataTable();
+
+            //Deserialize Data
+            List<Event> Events = JsonConvert.DeserializeObject<List<Event>>(File.ReadAllText(fileAddress));
+
+            //Add Columns
+            HistoryTable.Columns.Add("Date");
+            HistoryTable.Columns.Add("Note");
+            HistoryTable.Columns.Add("Cents Off");
+
+            //Add Value
+            if (new FileInfo(fileAddress).Length == 0)
+            {
+                dataGridView1.DataSource = null;
+            }
+            else
+            {
+                int i = 0;
+                foreach (var oItem in Events)
+                {
+                    HistoryTable.Rows.Add(new object[] { oItem.date, oItem.note, oItem.centOff });
+                }
+            }
+
+            //Load into DataGridView
+            dataGridView1.DataSource = HistoryTable;
+        }
+    }
+}
