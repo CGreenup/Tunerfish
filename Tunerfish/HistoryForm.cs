6bd6d503c98dc3b89d030b8e4c0ff2d4cb8cc400
qtran9775 <77847133+qtran9775@users.noreply.github.com>
2021-04-23 23:21:01 -0500

    Has added "using TunerFish"

--- Tunerfish/HistoryForm.cs
+++ Tunerfish/HistoryForm.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Windows.Forms;
 using Newtonsoft.Json;
 using System.IO;
+using TunerFish;
 
 namespace Tunerfish
 {
@@ -19,7 +18,6 @@ namespace Tunerfish
 
         private void HistoryForm_Load(object sender, EventArgs e)
         {
-            /*
             //Create Data Table
             DataTable HistoryTable = new DataTable();
 
@@ -38,16 +36,47 @@ namespace Tunerfish
             }
             else
             {
-                int i = 0;
                 foreach (var oItem in Events)
                 {
                     HistoryTable.Rows.Add(new object[] { oItem.date, oItem.note, oItem.centOff });
                 }
             }
-         
+
             //Load into DataGridView
             dataGridView1.DataSource = HistoryTable;
-            */
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Display Row of Cell and warning message
+            System.Text.StringBuilder messageBoxCS = new System.Text.StringBuilder();
+            messageBoxCS.AppendFormat("{0}:{1}", "You want to delete Row ", e.RowIndex);
+            messageBoxCS.AppendLine();
+            MessageBox.Show(messageBoxCS.ToString(), "Event Deletion");
+
+            //Deserialize data
+            List<Event> Events = JsonConvert.DeserializeObject<List<Event>>(File.ReadAllText(fileAddress));
+            Events.RemoveAt(e.RowIndex);
+
+            //Delete All text in file
+            File.WriteAllText(fileAddress, string.Empty);
+
+            //Serialize the new data
+            string json = JsonConvert.SerializeObject(Events, Formatting.Indented);
+            File.WriteAllText(fileAddress, json);
+
+            //Load Form
+            HistoryForm_Load(sender, e);
+            dataGridView1.Update();
+            dataGridView1.Refresh();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(fileAddress, string.Empty);
+            HistoryForm_Load(sender, e);
+            dataGridView1.Update();
+            dataGridView1.Refresh();
         }
     }
 }
